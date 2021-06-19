7b1210b5a99767a4820d2fe99473ff395502a6bf
venkatasai dholendar reddy <60260834+dholendar14@users.noreply.github.com>
2021-06-19 15:41:27 +0530

    intial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+opt = Options()
+opt.add_argument('--disable-blink-features=AutomationControlled')
+opt.add_argument('--start-maximized')
+opt.add_experimental_option("prefs", {
+
+    "profile.default_content_setting_values.media_stream_mic": 1,
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.geolocation": 0,
+    "profile.default_content_setting_values.notifications": 1
+})
+
+driver = webdriver.Chrome(options=opt, executable_path=r'C:\Program Files (x86)\chromedriver.exe')
+
+
+def google_signin():
+    driver.get("https://apps.google.com/intl/en/meet/")
+    sign_in = driver.find_element_by_xpath('/html/body/header/div[1]/div/div[3]/div[1]/div/span[1]/a').click()
+    email = driver.find_element_by_id("identifierId")
+    email.send_keys("cse17jn1a0575@gmail.com")
+    driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/span').click()
+    time.sleep(3)
+    password = driver.find_element_by_name("password")
+    password.send_keys("password") # replace the password
+    driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button').click()
+    time.sleep(10)
+
+
+def mic_off():
+    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div/span/span/div/div[1]/div').click()
+
+
+def camera_off():
+    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div/span/span/div/div').click()
+
+
+def meet_join():
+    meet_id = driver.find_element_by_xpath('//*[@id="i3"]')
+    meet_id.send_keys("kqd cwek rrr")
+    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/button/span').click()
+    time.sleep(5)
+    mic_off()
+    time.sleep(1)
+    camera_off()
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[2]/div/div[2]/div/div[1]/div[1]/span').click()
+
+
+if __name__ == '__main__':
+    google_signin()
+    meet_join()
+    time.sleep(80)
+    number = driver.find_element_by_class_name("uGOf1d")
