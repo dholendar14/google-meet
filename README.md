58258af3ae69db5762acc6e90a36ac97f456f80d
venkatasai dholendar reddy <60260834+dholendar14@users.noreply.github.com>
2021-06-20 07:26:26 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# google-meet
+# Google Meet Automation
+This is an python script which automate the `Google meet` login process and exit the meet if people in the meet are less than 15
+
+#### Requirements:
+
+
+    $ pip3 install selenium
+
+#### change in code:
+ 
+<p>
+    
+    meeting_id = " " # replace with google meet id
+    email.send_keys("cse17jn1a0575@gmail.com") #replace with your gmail
+    password.send_keys("csesvcn575#@!")  # replace with your password
+</p>
+
+> Note: No need to contain `-` in google meet id
+
+#### Installation:
+<p>
+
+    $ git clone https://github.com/dholendar14/google-meet.git
+    $ python3 main.py
+</p>
+    
--- main.py
+++ main.py
@@ -15,32 +15,36 @@ opt.add_experimental_option("prefs", {
 
 driver = webdriver.Chrome(options=opt, executable_path=r'C:\Program Files (x86)\chromedriver.exe')
 
+meeting_id = " "
 
 def google_signin():
     driver.get("https://apps.google.com/intl/en/meet/")
-    sign_in = driver.find_element_by_xpath('/html/body/header/div[1]/div/div[3]/div[1]/div/span[1]/a').click()
+    driver.find_element_by_xpath('/html/body/header/div[1]/div/div[3]/div[1]/div/span[1]/a').click()
     email = driver.find_element_by_id("identifierId")
     email.send_keys("cse17jn1a0575@gmail.com")
     driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button/span').click()
     time.sleep(5)
     password = driver.find_element_by_name("password")
-    password.send_keys("svcn575#@!") # replace the password
+    password.send_keys("csesvcn575#@!")  # replace the password
     driver.find_element_by_xpath('//*[@id="passwordNext"]/div/button').click()
     time.sleep(10)
 
 
 def mic_off():
-    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[1]/div/div/div/span/span/div/div[1]/div').click()
+    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div['
+                                 '1]/div/div[4]/div[1]/div/div/div/span/span/div/div[1]/div').click()
 
 
 def camera_off():
-    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div/span/span/div/div').click()
+    driver.find_element_by_xpath(
+        '//*[@id="yDmH0d"]/c-wiz/div/div/div[9]/div[3]/div/div/div[4]/div/div/div[1]/div[1]/div/div[4]/div[2]/div/div/span/span/div/div').click()
 
 
 def meet_join():
     meet_id = driver.find_element_by_xpath('//*[@id="i3"]')
-    meet_id.send_keys("amz gmgq ieh")
-    driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/button/span').click()
+    meet_id.send_keys("meeting_id")
+    driver.find_element_by_xpath(
+        '//*[@id="yDmH0d"]/c-wiz/div/div[2]/div/div[1]/div[3]/div/div[2]/div[2]/button/span').click()
     time.sleep(5)
     mic_off()
     time.sleep(1)
@@ -53,10 +57,12 @@ if __name__ == '__main__':
     google_signin()
     meet_join()
     time.sleep(100)
-    people = driver.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[3]/div[2]/div/div/div[2]/div/div')
+    people = driver.find_element_by_xpath(
+        '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[3]/div[2]/div/div/div[2]/div/div')
     while int(people.text) != 0:
         if int(people.text) < 15:
-            driver.find_element_by_xpath('//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[2]/div/div[6]/span/button').click()
+            driver.find_element_by_xpath(
+                '//*[@id="ow3"]/div[1]/div/div[9]/div[3]/div[10]/div[2]/div/div[6]/span/button').click()
             break
         else:
             time.sleep(10)
